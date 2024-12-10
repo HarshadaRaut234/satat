bf4517e4a8ee2f6e301f34220ae50650503de915
abhishekpythons <abhishekpythons@gmail.com>
2024-12-10 07:58:11 +0530

    adding models

--- satat_backend/satat_backend/tracker/models.py
+++ satat_backend/satat_backend/tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Satellite(models.Model):
+    norad_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    tle_line1 = models.CharField(max_length=100)
+    tle_line2 = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
