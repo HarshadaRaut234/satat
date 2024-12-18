e2d5cf1a665173e8df06d7431eed19937c3b0e95
abhishekpythons <abhishekpythons@gmail.com>
2024-12-18 10:17:30 +0530

    adding example of adding a single packet in models

--- satat_backend/satat_backend/decoder/views.py
+++ satat_backend/satat_backend/decoder/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .decode import *
+from .models import *
 
 def file_input(request):
     return render(request, 'input_file.html')
+
+def get_packet_by_index(data_df, summary_df, index):
+    packet = data_df[int(summary_df['packet_start'][index]):int(summary_df['packet_start'][index]+summary_df['length'][index])].astype('uint8')
+    return decode_packets(packet, summary_df['packet_type'][index])
 
 def ccsds_decoder(request):
     # decoded_value = main()
@@ -15,6 +20,8 @@ def ccsds_decoder(request):
 
     summary_df = summarize_data(data_df)
 
-    decoded_values = packetiser(data_df, summary_df)
-    
+    # decoded_values = packetiser(data_df, summary_df)
+    decoded_values = get_packet_by_index(data_df, summary_df, 1)
+    if decoded_values["CCSDSAPID"] ==1:
+        new_hk_packet = HkPacket.objects.create(Filename = file.name, **decoded_values)
     return JsonResponse(decoded_values, safe = False)
