05b1d3cfd58aa64abbba55ced6055610bc2eb7b7
HarshadaRaut234 <harshadaraut2004@gmail.com>
2024-12-24 22:13:20 +0530

    Changed the models.py, views.py and urls.py in tracker so that TLE for satellites is stored in the database and updated rather than getting it from Celestrack everytime.

--- satat_backend/satat_backend/tracker/urls.py
+++ satat_backend/satat_backend/tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('groundstation/', views.get_groundstation_position),
     path('satellite/<int:satellite_id>/positions/', views.get_satellite_position, name='satellite_positions'),
+    path('update-tle/', views.update_tle),
 ]
